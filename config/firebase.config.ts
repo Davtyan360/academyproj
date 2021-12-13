02bc40f4f60316ee1ba8838aa28c7006d832a49f
Yuri Davtyan <76738628+Davtyan360@users.noreply.github.com>
2021-12-13 17:26:01 +0400

    Add files via upload

---
+++ config/firebase.config.ts
@@ -0,0 +1,17 @@
+export default {
+  serviceAccount: {
+    type: "service_account",
+    project_id: "edith-plus-project",
+    private_key_id: "d9255af6b5cfb567942ad8c329c37c14a42582a8",
+    private_key:
+      "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDdVjhAO8T+md8c\nPPx2joxW4qHKLrgqc/Cbsgtx9kBsA5TPgfxy+eoS2ktoYV+LJsKlbn3vAnoasVn2\nC4V29ENS7PpQHqMOeJ0ZD9ytTsnifBcLHF38tdt6D4EvzK1v8YnoKGM4vz1Cs4AZ\nax2GY0RDKLOIdZgQvL3SBcUIV/fiE2xDbb7FuNMkDzMocnE2ADYCcjtr3xGYRgJp\nwYt1JX8cZpQC6xLBgSSSBip8ahi7appUL7lgognb7yXTOyHb4U/mruz37cgNZmMe\njrZ8lRl/VWTKhQx3Ll8D+Bx9v54gFP405Gx4lxMdgc5WNFlKVbQYnQSE/paEwoS0\nZ4Lgyd4rAgMBAAECggEABy6BAG+hRQLNbdiEHrhdDCIz69syrfVW8PZXgLoymxOI\n01QRA/oIjMI0MgQR5AX4F8foydl3V0DYEX54q2HiM37tksF6f7fcARKiYJIgCjZ7\n7J42tKS9B5ME6DkP7b8o5ly/RUhn3CWTy2a6WefJHRS97VEcF47vmwlAlO9+Yzyt\n/yFSx5fCz1HfcYMUm7l3Kf+qqKLCbTyPnwGG3u5LlSpIz+Qtn5HymkKu1hjfJpSx\nV9LqlimeZqnZ39lT522VVpVSFN/JL4YJ+HTPK1y5TH/I8P6BOwrUluCYSfeZv8aK\nK7cWHL3FFudjZI4vngfToBoS9Thna0O10YS6gNxJYQKBgQD/vwE8GvViIrjpfGG3\nF2U3Yk+Tz3UbVyfR/UEe/T46EpiF0pJDMc2Hb2SNSKnVQvEsAsja5w84D9Vw1mD+\ndA783Q+QzkO9NBJ78roHJHMPldpreP6Sek8BvtDEgWeo+6rUHRj85Qkz7Hrb0dzX\n7bCqHnY+aGKGyLzSHFqB3jekVwKBgQDdjnhbN7y+bGGpF+NkI80PxD6K5116+k24\n8K3MBQrvbJguFGiq9OqCJLL3TnTq0endH6jifma+zC5izueKVFfrCh4J/27vHPyG\nfIt/R5Z+mLVmy8t6W281DwpUKILsgyviVGSSRND1HRPumOU+6za2awmMq3wgxt6P\nxjt9UXgQTQKBgQCathOrJqvXKCYwqRv7xN1oOcLmKhQv6/Oc5tXkR1LDkshN/Gqa\n15/8FHkBtzl2V77J3w1ez3S3d2ypjCa1B8ShV6G4+e6koMGl1dqPw48PmJm/djya\no/7zkVl95sKsyUI4eznIk02T6bUREc3FmnbLEw55BnDEFN+VQ63E7oc+lQKBgHsD\ns8fR9rY0LzVX0iWzyxPJ/n/ZXon9mFHrMFRrKFsvjH9pTYQ+YEbKYaP2p+wJiTQB\ndB/t1tZxQAAUqXKgyf/yqiMa9FlHWt7BICMXeHPqDi55UQ0q32AybaW0qJzxZ9hO\ny1h7ZRnzfzGnKwH4T3SRkcfNHyqDwOdLRZSbO3KxAoGBANcESQVYDw5OVu8IbLXW\neGdCNb+apyVFbVgm3wY+1D0tWjJoKtjw3FglStdzlJpoSBcKp0WreAdqnxN2ieaO\nSRoJiSmC1jGl7jkGK7kb1b6ZeeBf0oAGFBMMyYMUswjapa8oUMjkIb0832BFH2I9\n/Sd2PykxbjMo8gNgii9VYM7R\n-----END PRIVATE KEY-----\n",
+    client_email:
+      "firebase-adminsdk-s249h@edith-plus-project.iam.gserviceaccount.com",
+    client_id: "107965107810991427357",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url:
+      "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-s249h%40edith-plus-project.iam.gserviceaccount.com",
+  },
+};
