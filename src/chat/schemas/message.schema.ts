f0eedddfb5b905eecc6041799a7c7db8d97e29b7
Yuri Davtyan <76738628+Davtyan360@users.noreply.github.com>
2021-12-21 13:17:06 +0400

    Update message.schema.ts

--- src/chat/schemas/message.schema.ts
+++ src/chat/schemas/message.schema.ts
@@ -1,25 +1,45 @@
 import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
 import * as mongoose from "mongoose";
+import {ApiProperty} from "@nestjs/swagger";
 
 export type MessageDocument = Message & Document;
 
 @Schema()
 export class Message {
+    @ApiProperty({
+        description: 'Message sender',
+    })
     @Prop({required: true})
     from: mongoose.Schema.Types.ObjectId
 
+    @ApiProperty({
+        description: 'Message taker',
+    })
     @Prop({required: true})
     to: mongoose.Schema.Types.ObjectId
 
+    @ApiProperty({
+        description: 'Message text',
+    })
     @Prop({required: true})
     text: string
 
+    @ApiProperty({
+        description: 'Create date',
+    })
     @Prop({required: true})
     createdAt: Date
 
+    @ApiProperty({
+        description: 'Update date',
+    })
     @Prop({required: true})
     updatedAt: Date
 
+
+    @ApiProperty({
+        description: 'Seen users',
+    })
     @Prop()
     seen: [mongoose.Schema.Types.ObjectId]
 }
