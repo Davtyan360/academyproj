c03037e07d8f728aaa1fdb37307e80fdc700bd58
Yuri Davtyan <76738628+Davtyan360@users.noreply.github.com>
2021-12-23 16:19:26 +0400

    Update user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from "@nestjs/mongoose";
 
 import { UserService } from "./user.service";
 import { UserSchema } from "./user.schema";
+import {UserController} from "./user.controller";
 
 @Module({
   //*******************user module for demo and testing purposes*******************
   imports: [MongooseModule.forFeature([{ name: "User", schema: UserSchema }])],
   exports: [UserService],
   providers: [UserService],
+  controllers:[UserController]
 })
 export class UserModule {}
